506bf742db73ab277766c4ba657b764b4296aa95
hamza-dev-tech <hamzadevtech01@gmail.com>
2024-08-08 11:26:03 +0500

    as

--- app/api/promotions/route.js
+++ app/api/promotions/route.js
@@ -1,15 +1,25 @@
 import prisma from "@/src/utils/connect";
 import { NextResponse } from "next/server";
 
-export async function GET() { // Change this to the HTTP method you expect, e.g., GET
+export async function GET() {
   try {
-    const promotions = await prisma.promotion.findMany({ orderBy: { createdAt: 'desc' } });
+    const promotions = await prisma.promotion.findMany({
+      orderBy: { createdAt: 'desc' }
+    });
 
-    return new NextResponse(JSON.stringify({ promotions }), { status: 200 });
+    return new NextResponse(
+      JSON.stringify({ promotions }),
+      {
+        status: 200,
+        headers: {
+          'Cache-Control': 'no-store', // Disable caching
+        },
+      }
+    );
   } catch (err) {
     console.error(err);
     return new NextResponse(
-      JSON.stringify({ message: "Something went wrong!" }),
+      JSON.stringify({ message: 'Something went wrong!' }),
       { status: 500 }
     );
   }
